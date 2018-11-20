6d70d160478af41c7892569fa380a56ba345c99d
wq <wuqi@weyee.com>
2018-11-20 18:04:12 +0800

    test emoji

--- app/src/main/java/com/letion/core/MainActivity.kt
+++ app/src/main/java/com/letion/core/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.letion.core
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvContent.text = printJson(parseJson(readJson()))
+    }
+
+    fun readJson(): String {
+        val inputStream = assets.open("emoji.json")
+        val length = inputStream.available()
+        val byteArray = ByteArray(length)
+        inputStream.read(byteArray)
+        inputStream.close()
+        return String(byteArray, Charset.defaultCharset())
+    }
+
+    fun parseJson(json: String): List<Emoji> {
+        val list = ArrayList<Emoji>()
+        val jsonArray = JSONObject(json).optJSONArray("list")
+        if (jsonArray != null) {
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.optJSONObject(i)
+                list.add(Emoji(jsonObject?.optString("name"), jsonObject.optInt("unicode")))
+            }
+        }
+        return list
+    }
+
+    fun printJson(array: List<Emoji>): String {
+        val builder = StringBuilder()
+        array.forEach {
+            builder.append(it.unicodeTemp)
+        }
+        return builder.toString()
+    }
+
+    data class Emoji(var name: String?, var unicode: Int) {
+        val unicodeTemp: String?
+            get() = String(Character.toChars(unicode))
     }
 }
